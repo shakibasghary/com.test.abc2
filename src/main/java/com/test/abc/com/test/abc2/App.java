44ea0afae7e91a05761bac2648f759c18835a464
shakibasghary <shakib.007@gmail.com>
2021-01-26 15:48:54 -0800

    xyz

---
+++ src/main/java/com/test/abc/com/test/abc2/App.java
@@ -0,0 +1,13 @@
+package com.test.abc.com.test.abc2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
